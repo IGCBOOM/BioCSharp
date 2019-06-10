7d844049fd6fb1b80b027ad60bb29c28a9bdd35d
IGCBOOM <caya1.carter1@gmail.com>
2019-06-10 13:19:00 -0500

    Started work on AbstractLocation

--- BioCSharp/Core/Sequence/Location/Template/AbstractLocation.cs
+++ BioCSharp/Core/Sequence/Location/Template/AbstractLocation.cs
@@ -8,7 +8,29 @@ namespace BioCSharp.Core.Sequence.Location.Template
     public class AbstractLocation : ILocation
     {
 
+        private static readonly long _serialVersionUID = 1L;
 
+        private IPoint _start;
+        private IPoint _end;
+        private Strand _strand;
+        private List<ILocation> _subLocations;
+        private bool _circular;
+        private bool _betweenCompounds;
+        private AccessionID _accession;
+
+        private bool _partialOn5prime = false;
+        private bool _partialOn3prime = false;
+
+        protected AbstractLocation() : base()
+        {
+        }
+
+        public AbstractLocation(IPoint start, IPoint end, Strand strand,
+            bool circular, bool betweenCompounds,
+            List<ILocation> subLocations) : this(start, end, strand, circular, betweenCompounds, null, subLocations);
+        {
+            
+        }
 
     }
 }
